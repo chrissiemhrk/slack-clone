30963480c8a51f90c5d7c9d93b5ff5291fed6a1a
chrissiemhrk <chrissiemhrk@gmail.com>
2021-08-07 12:47:06 +0200

    add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left"></div>
+      <div className="header__search"></div>
+      <div className="header__right"></div>
+    </div>
+  );
+}
+
+export default Header;
