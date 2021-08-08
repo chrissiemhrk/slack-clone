8ad57770a07d68827f4e65064b90caab6be05188
chrissiemhrk <chrissiemhrk@gmail.com>
2021-08-08 14:50:46 +0200

    add message component

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -2,12 +2,14 @@ import { InfoOutlined, StarBorderOutlined } from "@material-ui/icons";
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Message from "../../components/Message/Message";
 import db from "../../firebase";
 import Layout from "../../Layout/Layout";
 import "./Chat.css";
 function Chat() {
   const { channelId } = useParams();
   const [channelDetails, setChannelDetails] = useState(null);
+  const [channelMessages, setChannelMessages] = useState([]);
   useEffect(() => {
     if (channelId) {
       db.collection("channels")
@@ -16,7 +18,15 @@ function Chat() {
           setChannelDetails(snapshot.data());
         });
     }
+    db.collection("channels")
+      .doc(channelId)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+      .onSnapshot((snapshot) => {
+        setChannelMessages(snapshot.docs.map((doc) => doc.data()));
+      });
   }, [channelId]);
+
   return (
     <Layout>
       <div className="chat">
@@ -32,6 +42,16 @@ function Chat() {
               <InfoOutlined /> Details
             </p>
           </div>
+        </div>
+        <div className="chat__messages">
+          {channelMessages.map(({ message, user, userImage, timestamp }) => (
+            <Message
+              message={message}
+              timestamp={timestamp}
+              user={user}
+              userImage={userImage}
+            />
+          ))}
         </div>
       </div>
     </Layout>
